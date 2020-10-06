8eca787fff2a64e3f10977ee4aa3e86f623b9243
Henry-Ning <henryhuning@gmail.com>
2020-10-05 20:48:27 -0700

    58. Adding Gesture Detection with Flutter Button Widges 13min

--- lib/main.dart
+++ lib/main.dart
@@ -24,17 +24,21 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('Left');
+              },
               child: Image.asset('images/dice1.png'),
             ),
-            ),
+          ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('Right');
+              },
               child: Image.asset('images/dice1.png'),
             ),
-            ),
+          ),
         ],
       ),
     );
